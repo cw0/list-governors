848d1339b4347f30b807760c10081cf21f0791f9
cw0 <cwilson8706@gmail.com>
2021-05-21 14:18:07 -0400

    fix module pattern

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const fetchGovernors = require('./src/fetchGovernors');
+const fetchGovernor = require('./src/fetchGovernor');
 
-(async () => {
-  try {
-    const args = process.argv.slice(2);
-    const states = args[0].replace(/\s+/g, '').split(',');
-    const sortBy = args[1] ? args[1] : 'state';
-
-    const governors = await fetchGovernors(states, sortBy);
-
-    console.log({ governors });
-  } catch (error) {
-    console.error('An Error Occurred', error);
-  }
-})();
+module.exports = {
+  fetchGovernors,
+  fetchGovernor,
+};
